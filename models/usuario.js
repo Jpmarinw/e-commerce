49308b0d7cdf6bd2ee85c96cd79887dcd6a20ecc
Jpmarinw <joaopedrosouzamarinho@gmail.com>
2024-10-25 12:43:52 -0400

    Adicionando APIs para o Usuario

--- models/usuario.js
+++ models/usuario.js
@@ -1,12 +1,55 @@
 const mongoose = require("mongoose");
 
 const usuarioSchema = mongoose.Schema({
-  nome: String,
-  imagem: String,
-  qtdeEstoque: {
-    type: Number,
+  nome: {
+    type: String,
     required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  telefone: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  rua: {
+    type: String,
+    default: "",
+  },
+  cep: {
+    type: String,
+    default: "",
+  },
+  casa: {
+    type: String,
+    default: "",
+  },
+  cidade: {
+    type: String,
+    default: "",
+  },
+  pais: {
+    type: String,
+    default: "",
   },
 });
 
+usuarioSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+usuarioSchema.set("toJSON", {
+  virtuals: true,
+});
+
 exports.Usuario = mongoose.model("Usuario", usuarioSchema);
+exports.usuarioSchema = usuarioSchema;
--- routers/usuarios.js
+++ routers/usuarios.js
@@ -1,7 +1,9 @@
-const { Usuario } = require("../models/Usuario");
+const { Usuario } = require("../models/usuario");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
+// TRAZER A LISTA DE USUÁRIOS
 router.get(`/`, async (req, res) => {
   const usuarioList = await Usuario.find();
 
@@ -9,6 +11,27 @@ router.get(`/`, async (req, res) => {
     res.status(500).json({ sucess: false });
   }
   res.send(usuarioList);
+});
+
+// ADICIONAR USUÁRIOS
+router.post("/", async (req, res) => {
+  let usuario = new Usuario({
+    nome: req.body.nome,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.senha, 10),
+    telefone: req.body.telefone,
+    isAdmin: req.body.isAdmin,
+    rua: req.body.rua,
+    cep: req.body.cep,
+    casa: req.body.casa,
+    cidade: req.body.cidade,
+    pais: req.body.pais,
+  });
+
+  usuario = await usuario.save();
+
+  if (!usuario) return res.status(404).send("O usuário não foi criada!");
+  res.send(usuario);
 });
 
 module.exports = router;
