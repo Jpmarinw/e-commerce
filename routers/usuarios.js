a31efed68dc07d6d076f210690088de7dc0fced4
Jpmarinw <joaopedrosouzamarinho@gmail.com>
2024-10-25 19:19:15 -0400

    Autenticação de usuários por Login

--- routers/usuarios.js
+++ routers/usuarios.js
@@ -2,10 +2,11 @@ const { Usuario } = require("../models/usuario");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // TRAZER A LISTA DE USUÁRIOS
 router.get(`/`, async (req, res) => {
-  const usuarioList = await Usuario.find().select('-passwordHash');
+  const usuarioList = await Usuario.find().select("-passwordHash");
 
   if (!usuarioList) {
     res.status(500).json({ sucess: false });
@@ -15,7 +16,7 @@ router.get(`/`, async (req, res) => {
 
 // TRAZER USUÁRIO PELO ID
 router.get("/:id", async (req, res) => {
-  const usuario = await Usuario.findById(req.params.id).select('-passwordHash');
+  const usuario = await Usuario.findById(req.params.id).select("-passwordHash");
 
   if (!usuario) {
     res.status(500).json({
@@ -27,23 +28,61 @@ router.get("/:id", async (req, res) => {
 
 // ADICIONAR USUÁRIOS
 router.post("/", async (req, res) => {
-  let usuario = new Usuario({
-    nome: req.body.nome,
-    email: req.body.email,
-    passwordHash: bcrypt.hashSync(req.body.senha, 10),
-    telefone: req.body.telefone,
-    isAdmin: req.body.isAdmin,
-    rua: req.body.rua,
-    cep: req.body.cep,
-    casa: req.body.casa,
-    cidade: req.body.cidade,
-    pais: req.body.pais,
-  });
+  try {
+    const senhaHash = bcrypt.hashSync(req.body.senha, 10);
 
-  usuario = await usuario.save();
+    let usuario = new Usuario({
+      nome: req.body.nome,
+      email: req.body.email,
+      passwordHash: senhaHash,
+      telefone: req.body.telefone,
+      isAdmin: req.body.isAdmin,
+      rua: req.body.rua,
+      cep: req.body.cep,
+      casa: req.body.casa,
+      cidade: req.body.cidade,
+      pais: req.body.pais,
+    });
 
-  if (!usuario) return res.status(404).send("O usuário não foi criada!");
-  res.send(usuario);
+    usuario = await usuario.save();
+
+    if (!usuario) return res.status(404).send("O usuário não foi criado!");
+    res.send(usuario);
+  } catch (error) {
+    res.status(500).send("Erro no cadastro");
+  }
+});
+
+// LOGIN DE USUÁRIO
+router.post("/login", async (req, res) => {
+  try {
+    const usuario = await Usuario.findOne({ email: req.body.email });
+    const segredo = process.env.segredo;
+
+    if (!usuario) {
+      return res.status(400).send("Usuário não encontrado!");
+    }
+
+    const senhaValida = await bcrypt.compareSync(
+      req.body.senha,
+      usuario.passwordHash
+    );
+
+    if (!senhaValida) {
+      return res.status(400).send("Senha incorreta!");
+    }
+
+    const token = jwt.sign(
+      {
+        usuarioId: usuario.id,
+      },
+      segredo
+    );
+    res.status(200).send({ usuario: usuario.email, token: token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro no servidor");
+  }
 });
 
 // ALTERAR UM USUÁRIO
@@ -53,7 +92,6 @@ router.put("/:id", async (req, res) => {
     {
       nome: req.body.nome,
       email: req.body.email,
-      passwordHash: bcrypt.hashSync(req.body.senha, 10),
       telefone: req.body.telefone,
       isAdmin: req.body.isAdmin,
       rua: req.body.rua,
