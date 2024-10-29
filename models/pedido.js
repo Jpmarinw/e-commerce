4257e6fb6e2d9f35f17485457f1295b8f0dcf19e
Jpmarinw <joaopedrosouzamarinho@gmail.com>
2024-10-29 11:36:31 -0400

    Criação da tabela de pedidos e APIs para fazer os pedidos

--- models/pedido.js
+++ models/pedido.js
@@ -1,5 +1,60 @@
 const mongoose = require("mongoose");
 
-const pedidoSchema = mongoose.Schema({});
+const pedidoSchema = mongoose.Schema({
+  itensPedido: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "pedidoItem",
+      required: true,
+    },
+  ],
+  enderecoEnvio1: {
+    type: String,
+    required: true,
+  },
+  enderecoEnvio2: {
+    type: String,
+  },
+  cidade: {
+    type: String,
+    required: true,
+  },
+  cep: {
+    type: String,
+    required: true,
+  },
+  pais: {
+    type: String,
+    required: true,
+  },
+  telefone: {
+    type: String,
+    required: true,
+  },
+  precoTotal: {
+    type: Number,
+  },
+  usuario: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Usuario",
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "Pendente",
+  },
+  dataPedido: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+pedidoSchema.virtual("id").get(function () {
+    return this._id.toHexString();
+  });
+  
+  pedidoSchema.set("toJSON", {
+    virtuals: true,
+  });
 
 exports.Pedido = mongoose.model("Pedido", pedidoSchema);
--- routers/pedidos.js
+++ routers/pedidos.js
@@ -1,4 +1,5 @@
 const { Pedido } = require("../models/pedido");
+const { PedidoItem } = require("../models/pedidoItem");
 const express = require("express");
 const router = express.Router();
 
@@ -11,5 +12,57 @@ router.get(`/`, async (req, res) => {
   }
   res.send(pedidosList);
 });
+
+// CRIAR UM PEDIDO
+router.post("/", async (req, res) => {
+  const itensPedidoIds = Promise.all(
+    req.body.itensPedido.map(async (pedidoItem) => {
+      let newPedidoItem = new PedidoItem({
+        quantidade: pedidoItem.quantidade,
+        produto: pedidoItem.produto,
+      });
+
+      newPedidoItem = await newPedidoItem.save();
+
+      return newPedidoItem._id;
+    })
+  );
+
+  const itensPedidoResolved = await itensPedidoIds;
+
+  let pedido = new Pedido({
+    itensPedido: itensPedidoResolved,
+    enderecoEnvio1: req.body.enderecoEnvio1,
+    enderecoEnvio2: req.body.enderecoEnvio2,
+    cidade: req.body.cidade,
+    cep: req.body.cep,
+    pais: req.body.pais,
+    telefone: req.body.telefone,
+    precoTotal: req.body.precoTotal,
+    usuario: req.body.usuario,
+  });
+  pedido = await pedido.save();
+
+  if (!pedido) return res.status(404).send("O pedido não foi criado!");
+  res.send(pedido);
+});
+
+/*
+{
+"itensPedido" : [
+{
+"quantidade": 3,
+"produto": "id do produto"},
+{
+"quantidade": 2,
+"produto": "id do produto"}],
+"enderecoEnvio1": "Rua Aristofano Antony, 66, Petrópolis",
+"cidade": "Manaus",
+"cep": "69063-300",
+"pais": "Brasil",
+"telefone": "9299496945",
+"usuario": "id do usuario"
+}
+*/
 
 module.exports = router;
