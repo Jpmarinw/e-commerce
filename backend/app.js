d16e1f4dd8df6967fe3c275a6e968e8794d8d706
Jpmarinw <joaopedrosouzamarinho@gmail.com>
2024-11-20 21:58:03 -0400

    Comentando o código principal

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,15 @@
 // Sistema de e-commerce da equipe JAJIG
 
-const express = require("express");
+const express = require("express"); // Framework para criar API
 const app = express();
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv/config");
-const api = process.env.API_URL;
+const bodyParser = require("body-parser"); // Middleware para interpretar JSON
+const morgan = require("morgan"); // Middleware para registrar logs HTTP
+const mongoose = require("mongoose"); // Biblioteca para interagir com o MongoDB
+const cors = require("cors"); // Middleware que habilita o compartilhamento de recursos entre diferentes origens
+require("dotenv/config"); // Carrega variáveis de ambiente
+const api = process.env.API_URL; 
 
+// Importação das rotas
 const produtosRoutes = require("./routers/produtos");
 const categoriasRoutes = require("./routers/categorias");
 const usuariosRoutes = require("./routers/usuarios");
@@ -16,7 +17,7 @@ const pedidosRoutes = require("./routers/pedidos");
 const authJwt = require("./helpers/jwt");
 const errorHandler = require("./helpers/error-handler");
 
-//middlewares
+//middlewares globais
 app.use(cors());
 app.options("*", cors());
 app.use(bodyParser.json());
@@ -24,7 +25,7 @@ app.use(morgan("tiny"));
 app.use(authJwt());
 app.use(errorHandler);
 
-//Routers
+//Definições das rotas
 app.use(`${api}/produtos`, produtosRoutes);
 app.use(`${api}/categorias`, categoriasRoutes);
 app.use(`${api}/usuarios`, usuariosRoutes);
